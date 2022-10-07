ca9956d0d8224f888c6eac9bdcb1dea20d368eaa
putin <putin@putin.com>
2022-10-07 15:24:30 +0800

    feat: 调整多语言

--- src/diff-cmd.ts
+++ src/diff-cmd.ts
@@ -9,22 +9,15 @@ const diffCommon = (source, target) => {
     return;
   }
 
-  const { localePath } = getConfig();
-  const cnObj = require(path.join(rootPath, localePath, `${source}.json`));
-  const enObj = require(path.join(rootPath, localePath, `${target}.json`));
+  const { localePath, extName } = getConfig();
+  const cnObj = require(path.join(rootPath, localePath, source, extName));
+  const enObj = require(path.join(rootPath, localePath, target, extName));
   const diffObj = diff(cnObj, enObj);
-  const newFile = vscode.Uri.parse(
-    'untitled:' +
-      path.join(rootPath, localePath, `diff-${source}-${target}.json`)
-  );
-  vscode.workspace.openTextDocument(newFile).then((document) => {
+  const newFile = vscode.Uri.parse('untitled:' + path.join(rootPath, localePath, `diff-${source}-${target}.json`));
+  vscode.workspace.openTextDocument(newFile).then(document => {
     const edit = new vscode.WorkspaceEdit();
-    edit.insert(
-      newFile,
-      new vscode.Position(0, 0),
-      JSON.stringify(diffObj, null, '\t')
-    );
-    return vscode.workspace.applyEdit(edit).then((success) => {
+    edit.insert(newFile, new vscode.Position(0, 0), JSON.stringify(diffObj, null, '\t'));
+    return vscode.workspace.applyEdit(edit).then(success => {
       if (success) {
         vscode.window.showTextDocument(document);
       } else {
